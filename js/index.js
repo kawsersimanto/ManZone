54707f3dc176a6d79815e9acfb17384ca5875dd3
simu6 <semantos678@gmail.com>
2022-03-28 16:43:07 +0600

    added sticky navbar and hover effect on every cards

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let navbar = document.querySelector(".nav-bar");
+
+window.addEventListener("scroll", stickyNavbar);
+function stickyNavbar(){
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+}
