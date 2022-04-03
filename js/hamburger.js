341c9a74c74359f2ca44c140fa81a5c37e7f7ce0
simu6 <semantos678@gmail.com>
2022-04-03 23:15:21 +0600

    updated hamburger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.querySelector(".toggle-button");
 const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links ul li a");
+console.log(links);
 
 
 toggleBtn.addEventListener("click", showNavLinks);
@@ -8,3 +10,8 @@ function showNavLinks(e){
   navLinks.classList.toggle("display");
   toggleBtn.classList.toggle("display");
 }
+
+links.forEach(link => link.addEventListener("click", ()=>{
+  navLinks.classList.remove("display");
+  toggleBtn.classList.remove("display");
+}));
